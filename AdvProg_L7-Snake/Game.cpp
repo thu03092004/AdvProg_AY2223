1ba744c207ca314420bdd6e335bfdde2ebed5120
thu03092004 <nguyenthihoaithu3904@gmail.com>
2023-05-07 16:55:18 +0700

    update

--- AdvProg_L7-Snake/Game.cpp
+++ AdvProg_L7-Snake/Game.cpp
@@ -116,9 +116,10 @@ void Game::processUserInput(Direction direction)
 bool Game::canChange(Direction current, Direction next) const {
 	if (current == UP || current == DOWN) {
 		if (next == UP || next == DOWN) return 0;
-		return 1;
 	}
-	if (next == LEFT || current == RIGHT) return 0;
+	if (current == LEFT || current == RIGHT) {
+		if (next == LEFT || next == RIGHT) return 0;
+	}
 	return 1;
 }
 
